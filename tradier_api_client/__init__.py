ef8c3ff935452daa76dfa33732481f2a43456265
yunusindori <yunusindori@gmail.com>
2026-01-25 21:32:39 -0600

    Added URLs to example

--- tradier_api_client/__init__.py
+++ tradier_api_client/__init__.py
@@ -8,5 +8,5 @@ from tradier_api_client.streaming.streaming_client import StreamingClient
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = ["StreamingClient", "RestClient"]
